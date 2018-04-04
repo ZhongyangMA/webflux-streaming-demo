8f4071657adb242a05abe0c18d4bc3f7e432c9f3
mazhongyang <mazhongyang@renrenche.com>
2018-04-04 11:27:50 +0800

    entrance

--- src/main/java/com/solarwind/reactive/Application.java
+++ src/main/java/com/solarwind/reactive/Application.java
@@ -1,10 +1,17 @@
 package com.solarwind.reactive;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Zhongyang MA
  * Date: 2018/4/4
  * Time: 10:42
  */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
