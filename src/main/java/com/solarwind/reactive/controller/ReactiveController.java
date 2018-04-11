ee3df957ac2e6936c0f0767b563bf9238d9d6dcc
mazhongyang <mazhongyang@renrenche.com>
2018-04-11 18:10:39 +0800

    add handler

--- src/main/java/com/solarwind/reactive/controller/ReactiveController.java
+++ src/main/java/com/solarwind/reactive/controller/ReactiveController.java
@@ -1,15 +1,14 @@
 package com.solarwind.reactive.controller;
 
+import com.solarwind.reactive.handler.ExampleHandler;
 import javafx.util.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,23 +19,19 @@ import java.util.Date;
 @RestController
 @RequestMapping("/annotated")
 public class ReactiveController {
+    @Autowired
+    private ExampleHandler exampleHandler;
 
     // Example of returning a simple string to your browser
     @GetMapping("/test1")
     public Mono<String> test1() {
-        return Mono.just("test1");
+        return exampleHandler.test1();
     }
 
     // The server will push messages line by line to your browser - scrolling effect on your browser
     @GetMapping(value = "/test2", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Pair> test2() {
-        Flux<Pair> flux = Flux
-                .interval(Duration.ofMillis(1000))
-                .map(i -> {
-                    String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-                    return new Pair(now, i);
-                });
-        return flux;
+        return exampleHandler.test2();
     }
 
 }
--- src/main/java/com/solarwind/reactive/handler/ExampleHandler.java
+++ src/main/java/com/solarwind/reactive/handler/ExampleHandler.java
@@ -1,11 +1,16 @@
 package com.solarwind.reactive.handler;
 
+import javafx.util.Pair;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,6 +20,20 @@ import reactor.core.publisher.Mono;
  */
 @Component
 public class ExampleHandler {
+
+    public Mono<String> test1() {
+        return Mono.just("test1: return a simple string");
+    }
+
+    public Flux<Pair> test2() {
+        Flux<Pair> flux = Flux
+                .interval(Duration.ofMillis(1000))
+                .map(i -> {
+                    String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+                    return new Pair(now, i);
+                });
+        return flux;
+    }
 
     public Mono<ServerResponse> funcTest1(ServerRequest request) {
         return ServerResponse.ok()
